b09546158d7f5f46b74e12892a76a1cdd54ba68c
arianna-hub <71047618+arianna-hub@users.noreply.github.com>
2020-10-08 10:31:05 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onGesture(Gesture.Shake, function () {
+	
+})
+music.playTone(311, music.beat(BeatFraction.Whole))
+music.playMelody("E C5 E C E D F A ", 120)
+music.playMelody("B C5 A G F G A F ", 120)
+music.playMelody("E C5 G A F E G B ", 120)
+basic.forever(function () {
+	
+})
